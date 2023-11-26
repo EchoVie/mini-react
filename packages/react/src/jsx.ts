d0617d8e681ee6f4c20e007a5f5370d8055582a3
18720551903 <Echo981022@163.com>
2023-11-26 08:27:25 +0800

    feat: temp

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,82 @@
+import { ElementType, Ref, Key, Props } from 'shared/ReactTypes';
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+
+const ReactElement = (
+  type: ElementType,
+  key: Key,
+  ref: Ref,
+  props: Props
+) => {
+  return {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type,
+    ref,
+    key,
+    props
+  };
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+  let key: any = null;
+  let ref: Ref = null;
+  const props: Props = {};
+
+  for (const prop in config) {
+    const val = config[prop];
+    if (prop === 'key') {
+      if (val !== undefined) {
+        key = '' + val;
+      }
+      continue;
+    }
+
+    if (prop === 'ref') {
+      if (val !== undefined) {
+        ref = val;
+      }
+      continue;
+    }
+
+    if ({}.hasOwnProperty.call(config, prop)) {
+      props[prop] = val;
+    }
+  }
+
+  const maybeChildrenLength = maybeChildren.length;
+  if (maybeChildrenLength) {
+    if (maybeChildrenLength === 1) {
+      props.children = maybeChildren[0];
+    } else {
+      props.children = maybeChildren;
+    }
+  }
+
+  return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = (type: ElementType, config: any) => {
+  let key: any = null;
+  let ref: Ref = null;
+  const props: Props = {};
+
+  for (const prop in config) {
+    const val = config[prop];
+    if (prop === 'key') {
+      if (val !== undefined) {
+        key = '' + val;
+      }
+    }
+
+    if (prop === 'ref') {
+      if (val !== undefined) {
+        ref = val;
+      }
+    }
+
+    if ({}.hasOwnProperty.call(config, prop)) {
+      props[prop] = val;
+    }
+  }
+
+  return ReactElement(type, key, ref, props);
+};
---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,40 @@
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import ts from 'rollup-plugin-typescript2';
+import cjs from '@rollup/plugin-commonjs';
+import clear from 'rollup-plugin-clear';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { resolvePkgPath, getPackageJSON } from './utils';
+
+const { name, module } = getPackageJSON('react');
+const pkgPath = resolvePkgPath(name);
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default {
+  input: `${pkgPath}/${module}`,
+  output: {
+    file: `${pkgDistPath}/index.js`,
+    name: 'index',
+    format: 'umd'
+  },
+  plugins: [
+    nodeResolve({
+      exportConditions: ['node'],
+      extensions: ['.ts', '.mjs', '.js', '.json', '.node']
+    }),
+    ts({}),
+    cjs(),
+    generatePackageJson({
+      inputFolder: pkgPath,
+      outputFolder: pkgDistPath,
+      baseContents: ({ name, description, version }) => ({
+        name,
+        description,
+        version,
+        main: 'index.js'
+      })
+    }),
+    clear({
+      targets: ['dist']
+    })
+  ]
+};
